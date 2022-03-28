fe95115bd59778ea64788574243bf69e3cffff7b
juheehwang <zoiia6263@gmail.com>
2022-03-28 16:48:57 +0900

    Account 메소드 추가

--- gittest/src/test/Account.java
+++ gittest/src/test/Account.java
@@ -10,6 +10,11 @@ import lombok.ToString;
 public class Account {
 	private String ano;	
 	private int balance;
+	private String owner;
+	
+	public void deposit() {
+		System.out.println("deposit");
+	}
 	
 
 }
